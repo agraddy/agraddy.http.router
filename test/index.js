957f6ee1e1b6ad0f057ffa013293703966b49202
Anthony Graddy <anthony@locationq.com>
2016-12-07 00:56:54 +0000

    Working

--- test/index.js
+++ test/index.js
@@ -2,9 +2,29 @@ var test = require('tape');
 
 var mod = require('../');
 
-test('overall', function(t) {
-	t.equal('result', mod.main('result'));
+function home(req, res) {
+	return 'home';
+}
+function about(req, res) {
+	return 'about';
+}
+
+mod.routes([{'/': home}]);
+mod.add('/about', about);
+
+test('routing', function(t) {
+	t.equal(Object.keys(mod._routes).length, 2);
+	t.equal(mod.handler({url: '/about'}, null), 'about');
 	t.end();
 });
+
+// Add test for 200, 404, etc: mod.add(200, default);
+/*
+test('failure', function(t) {
+	t.equal(mod.handler({url: '/missing'}, null), 'about');
+	t.end();
+});
+*/
+
 
 
